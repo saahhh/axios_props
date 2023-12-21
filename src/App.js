25b94df49f8679bf2b057a9eaee019a5850e8767
saahhh <143372306+saahhh@users.noreply.github.com>
2023-12-21 16:37:54 +0900

    axios

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import DogInfo from './DogInfo';
+import UserList from './UserExam';
+import PostComment from './PostComment'
+
+function App() {
+  return (
+    <div className="App">
+      {/*<MovieList /> */}
+      {/*<PostAPI /> */}
+      {/* <DogInfo />*/}
+     {/* <UserList />*/}
+     <PostComment />
+    </div>
+  );
+}
+
+export default App;
